57b7ae0f5931b5cf664157c0122eb84984612cd0
mariahelenanoren <marialinder97@hotmail.com>
2021-05-18 15:08:14 +0200

    added userlist

---
+++ client/src/componenets/Admin/UserList.tsx
@@ -0,0 +1,29 @@
+import { List } from "antd";
+import UserListItem from "./UserListItem";
+
+export default function UserList() {
+  const users = [
+    {
+      firstname: "Maria",
+      lastname: "Norén",
+      email: "marialinder97@hotmail.com",
+      password: "123",
+      role: "admin" as "admin",
+    },
+    {
+      firstname: "Maria2",
+      lastname: "Norén2",
+      email: "marialinder97@hotmail.com2",
+      password: "1232",
+      role: "customer" as "customer",
+    },
+  ];
+
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={users}
+      renderItem={(user) => <UserListItem user={user} />}
+    ></List>
+  );
+}
---
+++ client/src/componenets/Admin/UserListItem.tsx
@@ -0,0 +1,33 @@
+import { List, Select } from "antd";
+import { User } from "../../interfaces";
+
+const { Option } = Select;
+
+interface Props {
+  user: User;
+}
+
+export default function UserListItem(props: Props) {
+  const { firstname, lastname, email, role } = props.user;
+
+  const handleChange = (value: string) => {
+    console.log(value);
+  };
+
+  return (
+    <List.Item
+      actions={[
+        <Select
+          defaultValue={role}
+          style={{ width: 120, right: 0 }}
+          onChange={handleChange}
+        >
+          <Option value="customer">Customer</Option>
+          <Option value="admin">Admin</Option>
+        </Select>,
+      ]}
+    >
+      <List.Item.Meta title={`${firstname} ${lastname}`} description={email} />
+    </List.Item>
+  );
+}
