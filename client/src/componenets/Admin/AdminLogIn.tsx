784ab898f04f4b9eba0ce8349328620632e7064d
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-05-18 17:35:02 +0200

    Layout log in page.

--- client/src/componenets/Admin/AdminLogIn.tsx
+++ client/src/componenets/Admin/AdminLogIn.tsx
@@ -1,22 +1,7 @@
-import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Checkbox } from 'antd';
 import { CSSProperties, Component } from 'react';
 import { Link } from 'react-router-dom';
-
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-
-const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
-};
 
 class AdminLogIn extends Component {
 
@@ -27,82 +12,126 @@ class AdminLogIn extends Component {
   onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
-    
+
   render() {
     return (
-      <div> 
-        <Row style={ContainerStyle}>
-          <Col span={24} style={columnStyle}>
-            <h1 style={{display: 'flex', justifyContent: 'center', fontWeight: 'bold'}}>LOG IN </h1>
-            <Form
-              {...layout}
-              name="basic"
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={this.onFinish}
-              onFinishFailed={this.onFinishFailed}
+      <main style={backgroundImage}>
+        <div style={formContainerStyle}>
+          <h1 style={titleStyle}
+          >
+            LOG IN
+          </h1>
+          <Form
+            name="login"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
+          >
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your email',
+                },
+              ]}
             >
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your username!',
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-      
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your password!',
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-      
-              <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-        
-              <Form.Item {...tailLayout}>        
-                <Link to={'/admin-list'}>
-                  <Button type="primary" htmlType="submit" style={buttonStyle}>
-                    Log in 
-                  </Button> 
-                </Link> 
-              </Form.Item>
-            </Form>
-          </Col>
-        </Row>
-      </div> 
+              <Input
+                prefix={<UserOutlined style={inputIconStyle}/>}
+                placeholder='E-mail'
+                style={{padding: '.8rem'}}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password',
+                },
+              ]}
+            >
+              <Input 
+                prefix={<LockOutlined style={inputIconStyle}/>} 
+                type='password' 
+                placeholder='Password'
+                style={{padding: '.8rem'}} 
+              />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item>
+              <Link to={'/admin-list'}>
+                <Button 
+                  type="primary" 
+                  htmlType="submit"
+                  style={buttonStyle}
+                >
+                  Log in
+                </Button>
+              </Link>
+            </Form.Item>
+            <Link to='/'> 
+              <p style={registerLink}>
+                Register now
+              </p> 
+            </Link>
+          </Form>
+        </div>
+      </main>
     );
   }
 }
 
-const ContainerStyle: CSSProperties = {
+const backgroundImage: CSSProperties = {
   display: 'flex',
-  justifyContent: 'space-around',
-  alignItems: 'space-around',
-  width: '60%',
-  margin: 'auto',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  width: '100%',
+  backgroundImage: 'url("https://github.com/feliciavonbraun/fashion-store/blob/master/client/src/assets/carousel1.png?raw=true")',
+
+  backgroundSize: 'cover',
 }
 
-const columnStyle: CSSProperties = {
-  marginTop: '14rem',
-  marginBottom: '3rem',
+const formContainerStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  maxWidth: '40rem',
+  padding: '3rem 1rem',
+  backgroundColor: '#FFFFFFE5',
+}
+
+const titleStyle: CSSProperties = {
+  textAlign: 'center',
+  fontWeight: 'bold',
+  marginBottom: '2rem',
+}
+
+const inputIconStyle: CSSProperties = {
+  fontSize: '1.2rem',
+  marginRight: '1rem'
 }
 
 const buttonStyle: CSSProperties = {
-  marginBottom: '10rem'
+  width: '100%',
+  fontSize: '1rem',
+  height: '2.5rem',
 }
 
-export default AdminLogIn; 
+const registerLink: CSSProperties = {
+  textAlign: 'center',
+  fontSize: '1rem',
+  fontWeight: 500,
+}
+
+export default AdminLogIn;
