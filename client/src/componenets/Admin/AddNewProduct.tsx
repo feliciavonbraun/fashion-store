7043816ab72e36c930715e20248848eb5f1a1b60
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-05-19 12:13:37 +0200

    renamed admin-list to product-list

--- client/src/componenets/Admin/AddNewProduct.tsx
+++ client/src/componenets/Admin/AddNewProduct.tsx
@@ -53,7 +53,7 @@ class AddNewProduct extends Component<Props, State> {
     newProduct.id = Math.max(...existingProducts.map((item: Product) => item.id)) + 1;
     existingProducts.push(newProduct)
     localStorage.setItem('products', JSON.stringify(existingProducts));
-    this.props.history.push('/admin-list');
+    this.props.history.push('/product-list');
     this.setState({ buttonSaveLoading: false });
   };
 
