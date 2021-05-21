4fcaa1c5374c7fe84d84c1c34a0519aba7400ad6
Mikaela Andersson <mikaelaaand@gmail.com>
2021-05-21 14:51:12 +0200

    changed model

--- server/resources/userResource/userModel.js
+++ server/resources/userResource/userModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    firstname: {type: String},
-    lastname: {type: String},
-    email: {type: String},
-    password: {type: String},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
     role: {type: String},
-    isLoggedIn: {type: Boolean},
+    adminRequest: {type: Boolean, required: true}
 });
 
 const UserModel = mongoose.model('user', UserSchema);
