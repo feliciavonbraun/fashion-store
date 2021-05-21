d7f7bd2da4d0501177e30eea7e46f55897094df5
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-05-21 14:48:24 +0200

    orderCOntext working in console

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,31 +9,34 @@ import Navbar from "./componenets/Navbar";
 import OrderSuccessMessage from "./componenets/OrderSuccess/OrderSuccessMessage";
 import ProductDetails from "./componenets/ProductDetails/ProductDetails";
 import StartPageView from "./componenets/StartPage/StartPageView";
-import CartProvider from "./contexts/CartContext";
 import ScrollToTop from "./componenets/ScrollToTop";
 import AddNewProduct from "./componenets/Admin/AddNewProduct";
+import CartProvider from "./contexts/CartContext";
 import ProductProvider from "./contexts/ProductContext";
+import OrderProvider from "./contexts/OrderContext";
 
 function App() {
   return (
     <ProductProvider>
-      <CartProvider>
-        <Router>
-          <ScrollToTop />
-          <Navbar />
-          <Switch>
-            <Route path="/product/:id" component={ProductDetails} />
-            <Route path="/ordersuccess" component={OrderSuccessMessage} />
-            <Route exact path="/" component={StartPageView} />
-            <Route path="/cart" component={CartView} />
-            <Route path="/login" component={LogIn} />
-            <Route path="/sidebar" component={Sidebar} />
-            <Route path="/add-product" component={AddNewProduct} />
-            <Route path="/edit-product/:id" component={AdminEditDetails} />
-          </Switch>
-          <Footer2 />
-        </Router>
-      </CartProvider>
+      <OrderProvider>
+        <CartProvider>
+          <Router>
+            <ScrollToTop />
+            <Navbar />
+            <Switch>
+              <Route path="/product/:id" component={ProductDetails} />
+              <Route path="/ordersuccess" component={OrderSuccessMessage} />
+              <Route exact path="/" component={StartPageView} />
+              <Route path="/cart" component={CartView} />
+              <Route path="/login" component={LogIn} />
+              <Route path="/sidebar" component={Sidebar} />
+              <Route path="/add-product" component={AddNewProduct} />
+              <Route path="/edit-product/:id" component={AdminEditDetails} />
+            </Switch>
+            <Footer2 />
+          </Router>
+        </CartProvider>
+      </OrderProvider>
     </ProductProvider>
   );
 }
