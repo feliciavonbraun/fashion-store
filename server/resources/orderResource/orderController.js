bb545d66efbb4f91dd425253c6a1fd16a67dff31
mariahelenanoren <marialinder97@hotmail.com>
2021-05-25 10:28:18 +0200

    new order updates product qty

--- server/resources/orderResource/orderController.js
+++ server/resources/orderResource/orderController.js
@@ -1,4 +1,5 @@
 const OrderModel = require('./orderModel');
+const ProductModel = require('../productResource/productModel');
 
 exports.getAllOrders = async (req, res) => {
     const docs = await OrderModel.find({});
@@ -6,18 +7,29 @@ exports.getAllOrders = async (req, res) => {
 };
 
 exports.getOneOrder = async (req, res) => {
-    const {_id} = req.body;
+    const { _id } = req.body;
     const doc = await OrderModel.find({ _id: _id });
     res.status(200).json(doc);
 };
 
 exports.newOrder = async (req, res) => {
+    const { orderItem } = req.body;
     const doc = await OrderModel.create(req.body);
+
+    /* UPDATES PRODUCT QUANTITY FOR EVERY PRODUCT IN ORDER */
+    for (const item of orderItem) {
+        const { product } = item;
+        await ProductModel.updateOne(
+            { _id: product._id },
+            { qty: product.qty - 1 }
+        );
+    }
+
     res.status(201).json(doc);
 };
 
 exports.updateOrder = async (req, res) => {
     const { _id } = req.body;
-    const doc = await OrderModel.replaceOne({ _id: _id }, {...req.body});
+    const doc = await OrderModel.replaceOne({ _id: _id }, { ...req.body });
     res.status(201).json(doc);
 };
