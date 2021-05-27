58c83cc3a80d1d8e08412f3ce3f3823aaee61e76
mariahelenanoren <marialinder97@hotmail.com>
2021-05-27 11:10:17 +0200

    profile page can now be reloaded with redirection

--- server/resources/userResource/userRouter.js
+++ server/resources/userResource/userRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const userRouter = express.Router();
 const controller = require('./userController');
-const { adminSecure, secure } = require('../../middleware/auth');
+const { adminSecure, secure, userSecure } = require('../../middleware/auth');
 
 // Lägg till secure
 userRouter.post('/api/user/register', controller.registerUser);
 userRouter.post('/api/user/login', controller.loginUser);
 userRouter.delete('/api/user/logout', secure, controller.logoutUser);
-userRouter.get('/api/user/auth', secure, controller.getLoggedinUser);
+userRouter.get('/api/user/auth', userSecure, controller.getLoggedinUser);
 
 /* ADMIN STUFF */
 userRouter.get('/api/user/admin', adminSecure, controller.getAllAdminRequests);
